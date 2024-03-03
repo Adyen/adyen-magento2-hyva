c9793c2a435eb42479a98ffd0bf2476b83b41638
Marjan <marjan.petkovski@onestic.com>
2024-03-03 17:27:09 +0100

    #18: Address error when switching between payment methods

--- Model/MethodList.php
+++ Model/MethodList.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace Adyen\Hyva\Model;
 
-use Adyen\Hyva\Api\Data\StoredCreditCardInterface;
-use Adyen\Hyva\Model\CreditCard\SavedCardsManager;
-
 class MethodList
 {
     public function __construct(
-        private SavedCardsManager $savedCardsManager,
         private $availableMethods = []
     ) {
     }
@@ -20,23 +16,6 @@ class MethodList
      */
     public function collectAvailableMethods(): array
     {
-        $storedCards = $this->getStoredCardsMethods();
-
-        return array_unique(array_merge($this->availableMethods, $storedCards));
-    }
-
-    /**
-     * @return array
-     */
-    private function getStoredCardsMethods(): array
-    {
-        $result = [];
-
-        /** @var StoredCreditCardInterface $storedCard */
-        foreach ($this->savedCardsManager->getStoredCards() as $storedCard) {
-            $result[] = $storedCard->getLayoutId();
-        }
-
-        return $result;
+        return $this->availableMethods;
     }
 }
