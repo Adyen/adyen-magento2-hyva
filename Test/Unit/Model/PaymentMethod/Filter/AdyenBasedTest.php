3dcb8e3aa0b91f67e222bb1fcef0696883e033a7
Marjan <marjan.petkovski@onestic.com>
2024-05-15 19:18:11 +0200

    #36: Unit tests: Reduce code duplication

--- Test/Unit/Model/PaymentMethod/Filter/AdyenBasedTest.php
+++ Test/Unit/Model/PaymentMethod/Filter/AdyenBasedTest.php
@@ -7,12 +7,28 @@ use PHPUnit\Framework\MockObject\MockObject;
 
 class AdyenBasedTest extends \PHPUnit\Framework\TestCase
 {
+    private MockObject $adyenCc;
+    private MockObject $adyenGPay;
+    private MockObject $adyenNotSupported;
     private MockObject $methodList;
 
     private AdyenBased $adyenBased;
 
     public function setUp(): void
     {
+        $this->adyenCc = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+        $this->adyenGPay = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+        $this->adyenNotSupported = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+
         $this->methodList = $this->createMock(\Adyen\Hyva\Model\MethodList::class);
 
         $this->adyenBased = new AdyenBased($this->methodList);
@@ -25,28 +41,15 @@ class AdyenBasedTest extends \PHPUnit\Framework\TestCase
             'adyen_googlepay'
         ];
 
-        $adyenCc = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-        $adyenGPay = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-        $adyenNotSupported = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-
         $list = [
-            'adyen_cc' => $adyenCc,
-            'adyen_googlepay' => $adyenGPay,
-            'adyen_unsuported' => $adyenNotSupported
+            'adyen_cc' => $this->adyenCc,
+            'adyen_googlepay' => $this->adyenGPay,
+            'adyen_unsuported' => $this->adyenNotSupported
         ];
 
-        $adyenCc->expects($this->exactly(2))->method('getCode')->willReturn('adyen_cc');
-        $adyenGPay->expects($this->exactly(2))->method('getCode')->willReturn('adyen_googlepay');
-        $adyenNotSupported->expects($this->exactly(2))->method('getCode')->willReturn('adyen_unsuported');
+        $this->adyenCc->expects($this->exactly(2))->method('getCode')->willReturn('adyen_cc');
+        $this->adyenGPay->expects($this->exactly(2))->method('getCode')->willReturn('adyen_googlepay');
+        $this->adyenNotSupported->expects($this->exactly(2))->method('getCode')->willReturn('adyen_unsuported');
 
         $this->methodList->expects($this->exactly(3))
             ->method('collectAvailableMethods')
--- Test/Unit/Model/PaymentMethod/Filter/AdyenConfiguredTest.php
+++ Test/Unit/Model/PaymentMethod/Filter/AdyenConfiguredTest.php
@@ -10,12 +10,28 @@ use PHPUnit\Framework\TestCase;
 
 class AdyenConfiguredTest extends TestCase
 {
+    private MockObject $adyenCc;
+    private MockObject $adyenGPay;
+    private MockObject $adyenNotConfigured;
     private MockObject $paymentMethods;
 
     private AdyenConfigured $adyenConfigured;
 
     public function setUp(): void
     {
+        $this->adyenCc = $this->getMockBuilder(PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+        $this->adyenGPay = $this->getMockBuilder(PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+        $this->adyenNotConfigured = $this->getMockBuilder(PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+
         $this->paymentMethods = $this->createMock(PaymentMethods::class);
 
         $this->adyenConfigured = new AdyenConfigured($this->paymentMethods);
@@ -37,28 +53,15 @@ class AdyenConfiguredTest extends TestCase
         $paymentMethodsResponse = ['paymentMethodsExtraDetails' => $paymentMethods];
         $paymentMethodsResponseSerialized = json_encode($paymentMethodsResponse);
 
-        $adyenCc = $this->getMockBuilder(PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-        $adyenGPay = $this->getMockBuilder(PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-        $adyenNotSupported = $this->getMockBuilder(PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-
         $list = [
-            'adyen_cc' => $adyenCc,
-            'adyen_googlepay' => $adyenGPay,
-            'adyen_unsuported' => $adyenNotSupported
+            'adyen_cc' => $this->adyenCc,
+            'adyen_googlepay' => $this->adyenGPay,
+            'adyen_not_configured' => $this->adyenNotConfigured
         ];
 
-        $adyenCc->expects($this->exactly(2))->method('getCode')->willReturn('adyen_cc');
-        $adyenGPay->expects($this->exactly(2))->method('getCode')->willReturn('adyen_googlepay');
-        $adyenNotSupported->expects($this->exactly(2))->method('getCode')->willReturn('adyen_unsuported');
+        $this->adyenCc->expects($this->exactly(2))->method('getCode')->willReturn('adyen_cc');
+        $this->adyenGPay->expects($this->exactly(2))->method('getCode')->willReturn('adyen_googlepay');
+        $this->adyenNotConfigured->expects($this->exactly(2))->method('getCode')->willReturn('adyen_not_configured');
 
         $this->paymentMethods->expects($this->once())
             ->method('getData')
@@ -71,6 +74,6 @@ class AdyenConfiguredTest extends TestCase
             array_keys($result)
         );
 
-        $this->assertArrayNotHasKey('adyen_unsuported', array_keys($result));
+        $this->assertArrayNotHasKey('adyen_not_configured', array_keys($result));
     }
 }
