5fd234c71553ac9860edd73649f6737c67f054df
Marjan <marjan.petkovski@onestic.com>
2024-05-15 18:57:19 +0200

    #36: Unit tests: Reduce StoredCardsTest.php code duplication

--- Test/Unit/Model/PaymentMethod/Filter/StoredCardsTest.php
+++ Test/Unit/Model/PaymentMethod/Filter/StoredCardsTest.php
@@ -8,12 +8,28 @@ use PHPUnit\Framework\MockObject\MockObject;
 
 class StoredCardsTest extends \PHPUnit\Framework\TestCase
 {
+    private MockObject $adyenCc;
+    private MockObject $adyenGPay;
+    private MockObject $adyenCcVault;
     private MockObject $storedCardsManager;
 
     private StoredCards $storedCards;
 
     public function setUp(): void
     {
+        $this->adyenCc = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+        $this->adyenGPay = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+        $this->adyenCcVault = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getCode', 'getTitle'])
+            ->getMock();
+
         $this->storedCardsManager = $this->createMock(StoredCardsManager::class);
 
         $this->storedCards = new StoredCards($this->storedCardsManager);
@@ -26,32 +42,13 @@ class StoredCardsTest extends \PHPUnit\Framework\TestCase
             'adyen_googlepay',
         ];
 
-        $adyenCc = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-        $adyenGPay = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-        $adyenCcVault = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-
-        $list = [
-            'adyen_cc' => $adyenCc,
-            'adyen_googlepay' => $adyenGPay,
-            'adyen_cc_vault' => $adyenCcVault
-        ];
-
-        $adyenCcVault->expects($this->once())->method('getCode')->willReturn('adyen_cc_vault');
+        $this->adyenCcVault->expects($this->once())->method('getCode')->willReturn('adyen_cc_vault');
 
         $this->storedCardsManager->expects($this->once())
             ->method('getStoredCards')
             ->willReturn([]);
 
-        $result = $this->storedCards->execute(123, $list);
+        $result = $this->storedCards->execute(123, $this->getList());
 
         $this->assertEquals(
             $supportedMethods,
@@ -69,36 +66,26 @@ class StoredCardsTest extends \PHPUnit\Framework\TestCase
             'adyen_cc_vault',
         ];
 
-        $adyenCc = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-        $adyenGPay = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-        $adyenCcVault = $this->getMockBuilder(\Magento\Quote\Api\Data\PaymentMethodInterface::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getCode', 'getTitle'])
-            ->getMock();
-
-        $list = [
-            'adyen_cc' => $adyenCc,
-            'adyen_googlepay' => $adyenGPay,
-            'adyen_cc_vault' => $adyenCcVault
-        ];
-
-        $adyenCcVault->expects($this->never())->method('getCode')->willReturn('adyen_cc_vault');
+        $this->adyenCcVault->expects($this->never())->method('getCode')->willReturn('adyen_cc_vault');
 
         $this->storedCardsManager->expects($this->once())
             ->method('getStoredCards')
             ->willReturn(['vaultCardA', 'vaultCardB']);
 
-        $result = $this->storedCards->execute(123, $list);
+        $result = $this->storedCards->execute(123, $this->getList());
 
         $this->assertEquals(
             $supportedMethods,
             array_keys($result)
         );
+    }
+
+    private function getList(): array
+    {
+        return [
+            'adyen_cc' => $this->adyenCc,
+            'adyen_googlepay' => $this->adyenGPay,
+            'adyen_cc_vault' => $this->adyenCcVault
+        ];
     }
 }
