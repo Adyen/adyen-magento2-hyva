ddd38090dfcd8715f7638bfdff87a2643fe3981f
sushmita <sushmita.thakur@adyen.com>
2024-08-21 17:16:51 +0200

    [ECP-9328] Enable hyva dependency installation

--- .github/scripts/install_magento.sh
+++ .github/scripts/install_magento.sh
@@ -112,28 +112,28 @@ else
 
 	echo "Installation completed"
 
-#	# Set up SSH config for gitlab.hyva.io
-#  echo "Host gitlab.hyva.io" >> /root/.ssh/config && \
-#  echo "  StrictHostKeyChecking no" >> /root/.ssh/config && \
-#  echo "  IdentityFile /root/.ssh/hyva_id_rsa" >> /root/.ssh/config && \
-#  chmod 600 /root/.ssh/config
-#
-#  # Configure composer
-#  echo "Configuring Composer with additional repositories"
-#  composer config repositories.hyva-themes/hyva-checkout git git@gitlab.hyva.io:hyva-checkout/checkout.git
-#  composer config repositories.hyva-themes/magento2-theme-module git git@gitlab.hyva.io:hyva-themes/magento2-theme-module.git
-#  composer config repositories.hyva-themes/magento2-reset-theme git git@gitlab.hyva.io:hyva-themes/magento2-reset-theme.git
-#  composer config repositories.hyva-themes/magento2-email-theme git git@gitlab.hyva.io:hyva-themes/magento2-email-module.git
-#  composer config repositories.hyva-default-theme git git@gitlab.hyva.io:hyva-themes/magento2-default-theme.git
-#
-#  # Install Hyva compatibility module
-#  echo "Installing Hyva compatibility module"
-#  composer require adyen/module-hyva-checkout:dev-develop
-#
-#  bin/magento module:enable --all
-#  bin/magento setup:di:compile
-#  bin/magento setup:static-content:deploy -f
-#  bin/magento cache:clean
+	# Set up SSH config for gitlab.hyva.io
+  echo "Host gitlab.hyva.io" >> /root/.ssh/config && \
+  echo "  StrictHostKeyChecking no" >> /root/.ssh/config && \
+  echo "  IdentityFile /root/.ssh/hyva_id_rsa" >> /root/.ssh/config && \
+  chmod 600 /root/.ssh/config
+
+  # Configure composer
+  echo "Configuring Composer with additional repositories"
+  composer config repositories.hyva-themes/hyva-checkout git git@gitlab.hyva.io:hyva-checkout/checkout.git
+  composer config repositories.hyva-themes/magento2-theme-module git git@gitlab.hyva.io:hyva-themes/magento2-theme-module.git
+  composer config repositories.hyva-themes/magento2-reset-theme git git@gitlab.hyva.io:hyva-themes/magento2-reset-theme.git
+  composer config repositories.hyva-themes/magento2-email-theme git git@gitlab.hyva.io:hyva-themes/magento2-email-module.git
+  composer config repositories.hyva-default-theme git git@gitlab.hyva.io:hyva-themes/magento2-default-theme.git
+
+  # Install Hyva compatibility module
+  echo "Installing Hyva compatibility module"
+  composer require adyen/module-hyva-checkout:dev-develop
+
+  bin/magento module:enable --all
+  bin/magento setup:di:compile
+  bin/magento setup:static-content:deploy -f
+  bin/magento cache:clean
 fi
 
 if [ "$DEPLOY_SAMPLEDATA" -eq 1 ]; then
