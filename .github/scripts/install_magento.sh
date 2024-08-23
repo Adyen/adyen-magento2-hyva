7e03bcbbc564bc981ef80627bfa334ea86b2e947
sushmita <sushmita.thakur@adyen.com>
2024-08-23 13:48:08 +0200

    [ECP-9328] remove reset theme

--- .github/scripts/install_magento.sh
+++ .github/scripts/install_magento.sh
@@ -166,7 +166,7 @@ EOF
   echo "Configuring Composer with additional repositories"
   composer config repositories.hyva-themes/hyva-checkout git git@gitlab.hyva.io:hyva-checkout/checkout.git
   composer config repositories.hyva-themes/magento2-theme-module git git@gitlab.hyva.io:hyva-themes/magento2-theme-module.git
-  composer config repositories.hyva-themes/magento2-reset-theme git git@gitlab.hyva.io:hyva-themes/magento2-reset-theme.git
+#  composer config repositories.hyva-themes/magento2-reset-theme git git@gitlab.hyva.io:hyva-themes/magento2-reset-theme.git
   composer config repositories.hyva-themes/magento2-email-theme git git@gitlab.hyva.io:hyva-themes/magento2-email-module.git
   composer config repositories.hyva-default-theme git git@gitlab.hyva.io:hyva-themes/magento2-default-theme.git
 
