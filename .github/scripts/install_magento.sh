97336ffd715e2c3674539288817911c623f16742
sushmita <sushmita.thakur@adyen.com>
2024-09-11 10:51:22 +0200

    [ECP-9328] Move Hyva installation to make file

--- .github/scripts/install_magento.sh
+++ .github/scripts/install_magento.sh
@@ -156,31 +156,31 @@ EOF
   php -f ../sample-data/dev/tools/build-sample-data.php -- --ce-source="/var/www/html"
   bin/magento setup:upgrade
 
-  # Set up SSH config for gitlab.hyva.io
-  echo "$SSH_PRIVATE_KEY" >> /root/.ssh/hyva_id_rsa
-  chmod 600 /root/.ssh/hyva_id_rsa
-  ssh-keyscan gitlab.hyva.io >> /root/.ssh/known_hosts
-  echo "Host gitlab.hyva.io" >> /root/.ssh/config && \
-  echo "  StrictHostKeyChecking no" >> /root/.ssh/config && \
-  echo "  IdentityFile /root/.ssh/hyva_id_rsa" >> /root/.ssh/config && \
-  chmod 600 /root/.ssh/config
-
-  # Configure composer
-  echo "Configuring Composer with additional repositories"
-  composer config repositories.hyva-themes/hyva-checkout git git@gitlab.hyva.io:hyva-checkout/checkout.git
-  composer config repositories.hyva-themes/magento2-theme-module git git@gitlab.hyva.io:hyva-themes/magento2-theme-module.git
-  composer config repositories.hyva-themes/magento2-reset-theme git git@gitlab.hyva.io:hyva-themes/magento2-reset-theme.git
-  composer config repositories.hyva-themes/magento2-email-theme git git@gitlab.hyva.io:hyva-themes/magento2-email-module.git
-  composer config repositories.hyva-default-theme git git@gitlab.hyva.io:hyva-themes/magento2-default-theme.git
-
-  # Install Hyva compatibility module
-  echo "Installing Hyva compatibility module"
-  composer require adyen/module-hyva-checkout:dev-develop
-
-  bin/magento module:enable --all
-  bin/magento setup:di:compile
-  bin/magento setup:static-content:deploy -f
-  bin/magento cache:clean
+#  # Set up SSH config for gitlab.hyva.io
+#  echo "$SSH_PRIVATE_KEY" >> /root/.ssh/hyva_id_rsa
+#  chmod 600 /root/.ssh/hyva_id_rsa
+#  ssh-keyscan gitlab.hyva.io >> /root/.ssh/known_hosts
+#  echo "Host gitlab.hyva.io" >> /root/.ssh/config && \
+#  echo "  StrictHostKeyChecking no" >> /root/.ssh/config && \
+#  echo "  IdentityFile /root/.ssh/hyva_id_rsa" >> /root/.ssh/config && \
+#  chmod 600 /root/.ssh/config
+#
+#  # Configure composer
+#  echo "Configuring Composer with additional repositories"
+#  composer config repositories.hyva-themes/hyva-checkout git git@gitlab.hyva.io:hyva-checkout/checkout.git
+#  composer config repositories.hyva-themes/magento2-theme-module git git@gitlab.hyva.io:hyva-themes/magento2-theme-module.git
+#  composer config repositories.hyva-themes/magento2-reset-theme git git@gitlab.hyva.io:hyva-themes/magento2-reset-theme.git
+#  composer config repositories.hyva-themes/magento2-email-theme git git@gitlab.hyva.io:hyva-themes/magento2-email-module.git
+#  composer config repositories.hyva-default-theme git git@gitlab.hyva.io:hyva-themes/magento2-default-theme.git
+#
+#  # Install Hyva compatibility module
+#  echo "Installing Hyva compatibility module"
+#  composer require adyen/module-hyva-checkout:dev-develop
+#
+#  bin/magento module:enable --all
+#  bin/magento setup:di:compile
+#  bin/magento setup:static-content:deploy -f
+#  bin/magento cache:clean
 fi
 
 ISSET_USE_SSL=$(bin/magento config:show web/secure/use_in_frontend)
