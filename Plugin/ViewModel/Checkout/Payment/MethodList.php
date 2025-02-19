d533abd3aea0d1b39afc36fd9b06b503b86a8154
Can Demiralp <can.demiralp@adyen.com>
2025-02-19 11:03:10 +0100

    Update the naming convention and return null-safe value

--- Plugin/ViewModel/Checkout/Payment/MethodList.php
+++ Plugin/ViewModel/Checkout/Payment/MethodList.php
@@ -5,24 +5,35 @@ namespace Adyen\Hyva\Plugin\ViewModel\Checkout\Payment;
 use Magento\Checkout\Model\Session as CheckoutSession;
 use Adyen\Payment\Helper\PaymentMethodsFilter;
 use Hyva\Checkout\ViewModel\Checkout\Payment\MethodList as HyvaMethodList;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class MethodList
 {
-    private $helper;
-
-    private $checkoutSession;
-
+    /**
+     * @param CheckoutSession $checkoutSession
+     * @param PaymentMethodsFilter $paymentMethodsFilterHelper
+     */
     public function __construct(
-        CheckoutSession $checkoutSession,
-        PaymentMethodsFilter $helper
-    ) {
-        $this->checkoutSession = $checkoutSession;
-        $this->helper = $helper;
-    }
+        private readonly CheckoutSession $checkoutSession,
+        private readonly PaymentMethodsFilter $paymentMethodsFilterHelper
+    ) { }
 
-    public function afterGetList(HyvaMethodList $subject, $result)
+    /**
+     * This plugin changes the sorting order of the payment methods on the checkout
+     * based on the sorting order of the payment methods on Adyen Customer Area.
+     *
+     * @param HyvaMethodList $subject
+     * @param $result
+     * @return array
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function afterGetList(HyvaMethodList $subject, $result): array
     {
-        list($result, $adyenPaymentMethodsResponse) = $this->helper->sortAndFilterPaymentMethods($result, $this->checkoutSession->getQuote());
-        return $result;
+        list($sortedMagentoPaymentMethods, $adyenPaymentMethodsResponse) = $this->paymentMethodsFilterHelper
+            ->sortAndFilterPaymentMethods($result, $this->checkoutSession->getQuote());
+
+        return $sortedMagentoPaymentMethods ?? $result;
     }
 }
