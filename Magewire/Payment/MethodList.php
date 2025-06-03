99a126c276ee424d9a700e3aa14921c5d6d2e89a
dunglv <dunglv@jajuma.de>
2025-06-03 14:30:30 +0700

    109 Update CSP migration

--- Magewire/Payment/MethodList.php
+++ Magewire/Payment/MethodList.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Adyen\Hyva\Magewire\Payment;
 
+use Hyva\Checkout\Magewire\Concern\Evaluatable;
+use Hyva\Checkout\Model\ConfigData\HyvaThemes\Developer\SystemConfigExperimental;
 use Hyva\Checkout\Model\Magewire\Component\EvaluationInterface;
 use Hyva\Checkout\Model\Magewire\Component\EvaluationResultFactory;
 use Hyva\Checkout\Model\Magewire\Component\EvaluationResultInterface;
 use Magento\Checkout\Model\Session as SessionCheckout;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Payment\Model\MethodInterface;
 use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Api\PaymentMethodManagementInterface;
 use Magewirephp\Magewire\Component;
+use Psr\Log\LoggerInterface;
 
 /**
  * Substitute for \Hyva\Checkout\Magewire\Checkout\Payment\MethodList
@@ -34,11 +40,30 @@ class MethodList extends Component implements EvaluationInterface
         'method' => 'Saving method'
     ];
 
+    protected SessionCheckout $sessionCheckout;
+    protected CartRepositoryInterface $cartRepository;
+    protected EvaluationResultFactory $evaluationResultFactory;
+    protected SystemConfigExperimental $experimentalHyvaCheckoutConfig;
+    protected PaymentMethodManagementInterface $paymentMethodManagement;
+
     public function __construct(
-        protected readonly SessionCheckout $sessionCheckout,
-        protected readonly CartRepositoryInterface $cartRepository,
-        protected readonly EvaluationResultFactory $evaluationResultFactory
+        SessionCheckout $sessionCheckout,
+        CartRepositoryInterface $cartRepository,
+        EvaluationResultFactory $evaluationResultFactory,
+        SystemConfigExperimental|null $experimentalHyvaCheckoutConfig = null,
+        PaymentMethodManagementInterface|null $paymentMethodManagement = null,
+        private LoggerInterface|null $logger = null
     ) {
+        $this->sessionCheckout = $sessionCheckout;
+        $this->cartRepository = $cartRepository;
+        $this->evaluationResultFactory = $evaluationResultFactory;
+
+        $this->experimentalHyvaCheckoutConfig = $experimentalHyvaCheckoutConfig
+            ?: ObjectManager::getInstance()->get(SystemConfigExperimental::class);
+        $this->paymentMethodManagement = $paymentMethodManagement
+            ?: ObjectManager::getInstance()->get(PaymentMethodManagementInterface::class);
+
+        $this->logger ??= ObjectManager::getInstance()->get(LoggerInterface::class);
     }
 
     public function boot(): void
@@ -61,9 +86,7 @@ class MethodList extends Component implements EvaluationInterface
             $quote->getPayment()->setMethod($value);
 
             $this->cartRepository->save($quote);
-
-            $this->dispatchBrowserEvent('checkout:payment:method-activate', ['method' => $value]);
-            $this->emit('payment_method_selected');
+            $this->emit('payment_method_selected', ['method' => $quote->getPayment()->getMethod()]);
         } catch (LocalizedException $exception) {
             $this->dispatchErrorMessage('Something went wrong while saving your payment preferences.');
         }
@@ -73,13 +96,75 @@ class MethodList extends Component implements EvaluationInterface
 
     public function evaluateCompletion(EvaluationResultFactory $resultFactory): EvaluationResultInterface
     {
-        if ($this->method === null) {
-            return $resultFactory->createErrorMessageEvent()
-                ->withCustomEvent('payment:method:error')
-                ->withMessage('The payment method is missing. Select the payment method and try again.');
+        // Run all sub-evaluations.
+        $this->evaluateSelection();
+
+        if ($this->evaluationBatch()->containsFailureResults()) {
+            return $this->evaluationBatch();
         }
 
-        return $resultFactory->createSuccess([], 'payment:method:success');
+        // Dispatches a success event to flag the component as completed.
+        return $this->evaluationBatch()->push(
+            $this->evaluationBatch()
+                 ->factory()
+                 ->createSuccess([], 'payment:method:success')
+                 ->dispatch()
+        );
+    }
+
+    private function evaluateSelection(): static
+    {
+        $method = $this->method;
+
+        try {
+            $quote = $this->sessionCheckout->getQuote();
+
+            // Grep all available payment methods.
+            $methods = $this->paymentMethodManagement->getList($quote->getId());
+            // Transform the numeric keys into the payment method code.
+            $methods = array_combine(array_map(fn (MethodInterface $object) => $object->getCode(), $methods), $methods);
+
+            /*
+             * Regardless of what happened during checkout, it’s possible that the previously selected
+             * payment method is no longer available, while other methods still are.
+             *
+             * In this specific situation, we want to inform the user and allow them to select
+             * a new available payment method, defaulting to the first available option.
+             */
+            if (count($methods) > 0 && is_string($method) && ! array_key_exists($method, $methods)) {
+                $this->evaluationBatch()->push(
+                    $this->evaluationBatch()->factory()
+
+                        ->createMessageDialog('Payment Method')
+                        ->withAlias('payment-method-not-available')
+                        ->withMessage('The previous active payment method is no longer available. To complete your order, please select a different payment option.')
+                        ->asInformally()
+                );
+
+                $method = null;
+            }
+        } catch (LocalizedException $exception) {
+            $this->evaluationBatch()->push(
+                $this->evaluationBatch()->factory()
+
+                     ->createMessageDialog('Sorry, an unexpected error occurred')
+                     ->presetAsTechnicalMalfunction()
+            );
+
+            return $this;
+        }
+
+        if ($method === null) {
+            $this->evaluationBatch()->push(
+                $this->evaluationBatch()->factory()
+
+                    ->createErrorMessageEvent()
+                    ->withCustomEvent('payment:method:error')
+                    ->withMessage('The payment method is missing. Select the payment method and try again.')
+            );
+        }
+
+        return $this;
     }
 }
 
